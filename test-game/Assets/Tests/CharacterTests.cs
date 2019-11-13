2be42134a92ce5c86d568ac7580e5909e62d4394
Perrald <32182928+Perrald@users.noreply.github.com>
2019-11-12 23:08:32 -0800

    Created first Unit Test
    Current Unit test doesn't build because the character class does not exist yet. This is part of the TDD cycle

---
+++ test-game/Assets/Tests/CharacterTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class CharacterTests
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void _New_Character_has_Full_Health()
+        {
+            // Use the Assert class to test conditions
+            Assert.AreEqual(1, character.CurrentHealth() / CharacterTests.MaxHealth());
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator CharacterTestsWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
